9e784bf7281d327361e8dcf0fc0c3cba637fc25a
EliasBoaventura <elias4144@hotmail.com>
2023-08-22 15:26:28 -0300

    nome variavel

--- exercicios02/01.py
+++ exercicios02/01.py
@@ -1,5 +1,5 @@
 quantidade_de_carros = 12
-valor_por_lavagame = 12.50
+valor_por_lavagem = 12.50
 
-calcular_ganhos = quantidade_de_carros * valor_por_lavagame
+calcular_ganhos = quantidade_de_carros * valor_por_lavagem
 print('Total de ganhos: R${:.2f}'.format(calcular_ganhos))
