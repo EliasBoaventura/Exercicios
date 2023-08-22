caaba615a23758dca8beafffe755bab2f2f07721
EliasBoaventura <elias4144@hotmail.com>
2023-08-22 14:03:45 -0300

    02, 03 e 04

--- 01.py
+++ 01.py
@@ -1,1 +1,1 @@
-print('hello, world!!!')
+print('Stephen hawking')
---
+++ 04.py
@@ -0,0 +1,21 @@
+print('Parabéns' + '!'*10)
+print('''Aqui vai um dinossauro!
+
+┈┈┈╭━━╮┈┈┈┈┈┈
+┈┈╭╯┊◣╰━━━━╮┈┈
+┈┈┃┊┊┊╱▽▽▽┛┈┈
+┈┈┃┊┊┊▏┈┈┈┈┈┈
+━━╯┊┊┊╲△△△┓┈┈
+┊┊┊┊╭━━━━━━╯┈┈
+
+''')
+print('Aqui vai um bolo! hu' + 10*'m'+'\n')
+print(' .'*10)
+print(' i'*10)
+print('#'*21)
+print('='*21)
+print('#'*21)
+print('='*21)
+print('#'*21)
+
+
