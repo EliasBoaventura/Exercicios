3fd5d7a0d07391c49fc622f8bc086d5014cf4586
EliasBoaventura <elias4144@hotmail.com>
2023-11-20 11:13:27 -0300

    .

--- exerciciosRunCodes/teste.py
+++ exerciciosRunCodes/teste.py
@@ -2,7 +2,7 @@ lista = ['a','c','d']
 
 memoria = lista[1]
 if isinstance(memoria, str):
-    if memoria.isnumeric:
+    if memoria.isnumeric():
         lista_ordenada = sorted(lista, key=float)
     else:
         lista_ordenada = sorted(lista)
